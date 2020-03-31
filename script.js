ba9e061f5a8518e7dd6dde720b46653a885c422b
Nilo E Rivera <47746418+nerivera@users.noreply.github.com>
2020-03-30 22:23:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 function output(r) {
-        document.getElementById("res").innerHTML = r;
-      }
+  document.getElementById("res").innerHTML = r;
+}
 
-      function genPi() {
-        let rolls = parseFloat(document.getElementById("rolls").value),
-          numcp = 0,
-          max = parseFloat(document.getElementById("max").value);
-        if (Number.isNaN(rolls) || rolls < 1 || !Number.isInteger(rolls) || !Number.isFinite(rolls))
-          return "The number of rolls must be a positive integer.";
-        if (Number.isNaN(max) || max < 2 || !Number.isInteger(max) || !Number.isFinite(max))
-          return "The maximum number must be an integer greater than 1.";
-        for (let i = 0; i < rolls; i++) {
-          if (coprime(Math.floor(Math.random() * max) + 1, Math.floor(Math.random() * 200) + 1))
-            numcp++;
-        }
-        return Math.sqrt(6 * rolls / numcp).toString();
-      }
+function genPi() {
+  let rolls = parseFloat(document.getElementById("rolls").value),
+    numcp = 0,
+    max = parseFloat(document.getElementById("max").value);
+  if (Number.isNaN(rolls) || rolls < 1 || !Number.isInteger(rolls) || !Number.isFinite(rolls))
+    return "The number of rolls must be a positive integer.";
+  if (Number.isNaN(max) || max < 2 || !Number.isInteger(max) || !Number.isFinite(max))
+    return "The maximum number must be an integer greater than 1.";
+  for (let i = 0; i < rolls; i++) {
+    if (coprime(Math.floor(Math.random() * max) + 1, Math.floor(Math.random() * 200) + 1))
+      numcp++;
+  }
+  return Math.sqrt(6 * rolls / numcp).toString();
+}
 
-      function coprime(a, b) {
-        while (a != b) {
-          if (a > b)
-            a = a - b;
-          else
-            b = b - a;
-          if(a < 1 || b < 1) {
-          	alert("a is " + a.toString() + " and b is " + b.toString());
-            return false;
-          }
-        }
-        return a == 1;
-      }
+function coprime(a, b) {
+  while (a != b) {
+    if (a > b)
+      a = a - b;
+    else
+      b = b - a;
+    if (a < 1 || b < 1) {
+      alert("a is " + a.toString() + " and b is " + b.toString());
+      return false;
+    }
+  }
+  return a == 1;
+}
